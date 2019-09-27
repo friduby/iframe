dc38b6cc6ecfbfcdee8247a5fb38f02e7307d25d
Friduby <friduby@gmail.com>
2019-09-28 02:38:46 +0330

    remove extra lines

--- iframe/src/components/Loader.js
+++ iframe/src/components/Loader.js
@@ -1,22 +1,6 @@
 import React from 'react';
 
 class Loader extends React.Component {
-    state = {
-        fill: 0,
-        dir: 1
-    }
-    componentDidMount() {
-        setInterval(function (){
-            let dir = this.state.dir;
-            if (this.state.fill < 1 && this.state.fill > 0)
-            {
-                let newVal = this.state.fill + 0.04*this.state.dir;
-                if (newVal > 1 || newVal < 0)
-                    dir = -dir;
-            }
-            this.setState({fill: (this.state.fill + 0.04*dir) % 1, dir:dir});
-        }.bind(this), 40);
-    }
 	render() {
         if (this.props.loading)
             return (
