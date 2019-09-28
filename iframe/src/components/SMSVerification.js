1bd85172109d21288e7b89231fe7bab0c4784683
bernard rieux <doctorrieuxishere@gmail.com>
2019-09-29 01:29:12 +0330

    little change

--- iframe/src/components/SMSVerification.js
+++ iframe/src/components/SMSVerification.js
@@ -5,7 +5,7 @@ import PhoneNumberInput from './inputs/PhoneNumberInput';
 class SMSVerification extends React.Component {
     render() {
         return (
-            <div className="uk-card uk-card-defau>
+            <div className="uk-card uk-card-default">
                 <h2>SMSVerification</h2>
                 <Form>
                     <p>This is the sms form</p>
