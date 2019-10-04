68b85e49b9faffb2d59ee74ef40027da015b718e
bernard rieux <doctorrieuxishere@gmail.com>
2019-10-04 03:41:16 +0330

    maxLength added to cardNumber

--- iframe/src/components/inputs/CardInput.js
+++ iframe/src/components/inputs/CardInput.js
@@ -8,7 +8,7 @@ class CardInput extends BaseInput {
     renderInput() {
         return (
             <div className="form-group" >
-                <input type={this.props.type || "number"} className="form-control uk-input uk-width-1-1" style={{float:'right', width:'100%'}} name={this.props.name} placeholder="_ _ _ _-_ _ _ _-_ _ _ _-_ _ _ _" pattern="(\d{4}-\d{4}-\d{4}-\d{4})" data-mask="0000-0000-0000-0000" required />
+                <input type={this.props.type || "text"} className="form-control uk-input uk-width-1-1" style={{float:'right', width:'100%'}} name={this.props.name} placeholder="_ _ _ _-_ _ _ _-_ _ _ _-_ _ _ _" pattern="(\d{4}-\d{4}-\d{4}-\d{4})" data-mask="0000-0000-0000-0000" maxLength="32" required />
             </div>
         );
     }
