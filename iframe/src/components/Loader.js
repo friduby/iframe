f59666a924f2181bf844227c905ef32f14385412
bernard rieux <doctorrieuxishere@gmail.com>
2019-10-05 03:11:42 +0330

    loader error direction

--- iframe/src/components/Loader.js
+++ iframe/src/components/Loader.js
@@ -8,7 +8,7 @@ class Loader extends React.Component {
                 <div>
                 <div class="iframe-loader uk-card uk-card-default" style={{textAlign:'center', marginTop:'120px'}}>
                     <i class="fas fa-exclamation-triangle" style={{margin:'10px auto',fontSize:'50px',color:'red'}}></i>
-                    <p style={{fontSize:'20px'}}>{this.props.error || "!خطا در بارگذاری اطلاعات"}</p>
+                    <p style={{fontSize:'20px', direction:'rtl'}}>{this.props.error || "!خطا در بارگذاری اطلاعات"}</p>
                     {this.props.onTryAgain &&
                         <a onClick={this.props.onTryAgain}>بازگشت</a>
                     }
