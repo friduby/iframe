d5fb0435966aeeedca8782c4650beb39d310952a
bernard rieux <doctorrieuxishere@gmail.com>
2019-10-18 15:36:33 +0330

    feedback added

---
+++ iframe/src/components/FeedBack.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Form from './Form';
+import './SMSVerification.css'
+import PhoneNumberInput from './inputs/PhoneNumberInput';
+import Zoom from 'react-reveal/Zoom';
+import Loader from './Loader';
+import FetchContext from '../utils/FetchContext';
+import CodeInput from './inputs/CodeInput';
+class FeedBack extends React.Component {
+
+    render() {
+        
+        return (
+            <div className="uk-grid" style={{border:'1px'}}>
+
+            </div>
+        );
+    }
+}
+
+
+export default FeedBack;
